73a15b5cdaf5276735eac3ab6f57120a930c227f
Lei Meng <lmeng2@seattleu.edu>
2023-04-20 17:45:41 -0700

    Updated ParksModel to match the JOSN file

--- src/Models/ParksModel.cs
+++ src/Models/ParksModel.cs
@@ -6,7 +6,7 @@ namespace LetsGoPark.WebSite.Models
     public class ParksModel
     {
         public string Id { get; set; }
-        public string Maker { get; set; }
+        public string Name { get; set; }
         
         [JsonPropertyName("img")]
         public string Image { get; set; }
@@ -14,6 +14,12 @@ namespace LetsGoPark.WebSite.Models
         public string Title { get; set; }
         public string Description { get; set; }
         public int[] Ratings { get; set; }
+        public string Address { get; set; }
+        public string Phone { get; set; }
+        public string Park_system { get; set; }
+        public string[] Activites { get; set; }
+        public string Map_brochure { get; set; }
+        public string Permits { get; set; }
 
         public override string ToString() => JsonSerializer.Serialize<ParksModel>(this);
 
