3d9e5747002d7be6f813ce1c93b665a8691d0791
namcnamara <nicholas.anthony.mcnamara@gmail.com>
2023-04-21 19:33:31 -0700

    -Added underline tags for header links in layout.html -Created Parks Index cshtml/cshtml.cs files

---
+++ src/Pages/ParksIndex.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+using LetsGoPark.WebSite.Models;
+using LetsGoPark.WebSite.Services;
+//Shatakshi
+namespace LetsGoPark.WebSite.Pages
+{
+    public class ParksIndexModel : PageModel
+    {
+        private readonly ILogger<ParksIndexModel> _logger;
+
+        public ParksIndexModel(ILogger<ParksIndexModel> logger,
+            JsonFileParksService parkService)
+        {
+            _logger = logger;
+            ParkService = parkService;
+        }
+        
+       
+
+        public JsonFileParksService ParkService { get; }
+        public IEnumerable<ParksModel> Parks { get; private set; }
+
+        public void OnGet()
+        {
+            Parks = ParkService.GetParks();
+        }
+    }
+}
