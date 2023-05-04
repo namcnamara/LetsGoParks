601ced7802d69cd8036b68cc9ca1bc4e0bc249c5
namcnamara <nicholas.anthony.mcnamara@gmail.com>
2023-05-03 23:56:42 -0700

    Added unit tests for pages

---
+++ UnitTests/Pages/Index.cshtml.Tests.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Logging;
+using NUnit.Framework;
+using Moq;
+using LetsGoPark.WebSite.Pages;
+using System.Collections.Generic;
+using LetsGoPark.WebSite.Models;
+using LetsGoPark.WebSite.Services;
+using Microsoft.AspNetCore.Hosting;
+
+namespace UnitTests.Pages
+{
+    public class IndexModelTests
+    {
+        [Test]
+        public void ModelIsRendered()
+        {
+            // Arrange
+            var loggerMock = new Mock<ILogger<IndexModel>>();
+            var envMock = new Mock<IWebHostEnvironment>();
+            var parkService = new JsonFileParksService(envMock.Object);
+
+            // Act
+            var model = new IndexModel(loggerMock.Object, parkService);
+
+            // Assert
+            Assert.NotNull(model);
+        }
+
+        
+    }
+}
---
+++ UnitTests/Pages/Update.cshtml.Tests.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.Logging;
+
+using NUnit.Framework;
+
+using Moq;
+
+using LetsGoPark.WebSite.Pages;
+using Microsoft.Extensions.Logging.Abstractions;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
+
+namespace UnitTests.Pages.Update
+{
+    public class UpdateModelTests
+    {
+        [Test]
+        public void ModelIsRendered()
+        {
+            // Arrange
+            var loggerMock = new Mock<ILogger<UpdateModel>>();
+
+            // Act
+            var model = new UpdateModel(loggerMock.Object);
+
+            // Assert
+            Assert.NotNull(model);
+           
+        }
+
+
+        
+    }
+}
