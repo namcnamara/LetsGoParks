7d5c776615b2134fab7da63ae61f74c0cfd9330d
Lei Meng <lmeng2@seattleu.edu>
2023-05-05 21:20:43 -0700

    Added unit tests to Index.cshtml.Tests.cs

--- UnitTests/Pages/Index.cshtml.Tests.cs
+++ UnitTests/Pages/Index.cshtml.Tests.cs
@@ -6,6 +6,8 @@ using System.Collections.Generic;
 using LetsGoPark.WebSite.Models;
 using LetsGoPark.WebSite.Services;
 using Microsoft.AspNetCore.Hosting;
+using System.IO;
+using System;
 
 namespace UnitTests.Pages
 {
@@ -26,6 +28,37 @@ namespace UnitTests.Pages
             Assert.NotNull(model);
         }
 
-        
+        [Test]
+        public void ParkService_Is_Loaded()
+        {
+            // Arrange
+            var loggerMock = new Mock<ILogger<IndexModel>>();
+            var envMock = new Mock<IWebHostEnvironment>();
+            var parkService = new JsonFileParksService(envMock.Object);
+
+            // Act
+            var model = new IndexModel(loggerMock.Object, parkService);
+
+            // Assert
+            Assert.NotNull(model.ParkService);
+        }
+
+        [Test]
+        public void Parks_Are_Loaded()
+        {
+            // Arrange
+            var loggerMock = new Mock<ILogger<IndexModel>>();
+            string wwwRootPath = Path.Combine(AppContext.BaseDirectory, "wwwroot");
+            var envMock = new Mock<IWebHostEnvironment>();
+            envMock.Setup(x => x.WebRootPath).Returns(wwwRootPath);
+            var parkService = new JsonFileParksService(envMock.Object);
+
+            // Act
+            var model = new IndexModel(loggerMock.Object, parkService);
+            model.OnGet();
+
+            // Assert
+            Assert.NotNull(model.Parks);
+        }
     }
 }
