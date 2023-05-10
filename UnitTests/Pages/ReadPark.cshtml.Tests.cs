238f7e24ff7ae24f1de642c5a90c52bdfda22e45
namcnamara <nicholas.anthony.mcnamara@gmail.com>
2023-05-10 00:32:10 -0700

    Added unit tests for ReadPark.Tests

---
+++ UnitTests/Pages/ReadPark.cshtml.Tests.cs
@@ -0,0 +1,50 @@
+﻿using System.Linq;
+
+using NUnit.Framework;
+
+using LetsGoPark.WebSite.Pages;
+using LetsGoPark.WebSite.Models;
+using LetsGoPark.WebSite.Services;
+using Microsoft.AspNetCore.Hosting;
+using Moq;
+using Microsoft.AspNetCore.Mvc;
+using static System.Net.Mime.MediaTypeNames;
+using System.Net;
+using System.Numerics;
+using System.Xml.Linq;
+using System;
+
+namespace UnitTests.Pages.ReadPark
+{
+    public class ReadParkTests
+    {
+        #region TestSetup
+        public static ReadParkModel pageModel;
+
+        [SetUp]
+        public void TestInitialize()
+        {
+            pageModel = new ReadParkModel(TestHelper.ParkService)
+            {
+            };
+        }
+
+        #endregion TestSetup
+
+        #region OnGet
+        [Test]
+        public void OnGet_Valid_Should_Return_Products()
+        {
+            // Arrange
+
+            // Act
+            pageModel.OnGet("LAKE SAMMAMISH STATE PARK");
+
+            // Assert
+            Assert.AreEqual(true, pageModel.ModelState.IsValid);
+            Assert.AreEqual("LAKE SAMMAMISH STATE PARK", pageModel.Park.Id);
+        }
+        #endregion OnGet
+    }
+}
+    
