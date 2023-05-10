a1710d9164ea1cadc58c38a78aaafe78550078dd
Lei Meng <lmeng2@seattleu.edu>
2023-05-10 11:17:52 -0700

    Created ParksList.razorTest.cs file; added TestBUnitHelper.cs

---
+++ UnitTests/Components/ParksList.razor.Tests.cs
@@ -0,0 +1,23 @@
+﻿using Bunit;
+using NUnit.Framework;
+
+using LetsGoPark.WebSite.Components;
+using Microsoft.Extensions.DependencyInjection;
+using LetsGoPark.WebSite.Services;
+using System.Linq;
+
+namespace UnitTests.Components
+{
+    public class ParksListTests : BunitTestContext
+    {
+        #region TestSetup
+
+        [SetUp]
+        public void TestInitialize()
+        {
+        }
+
+        #endregion TestSetup
+
+    }
+}
