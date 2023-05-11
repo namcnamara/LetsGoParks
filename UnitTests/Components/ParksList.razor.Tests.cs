704a15953802092c9bd36e4c4cc99363cf7fe000
namcnamara <nicholas.anthony.mcnamara@gmail.com>
2023-05-10 22:03:28 -0700

    Created Homepage.razor.Tests and add testing for ParkList content retrieval and Selectpark function

--- UnitTests/Components/ParksList.razor.Tests.cs
+++ UnitTests/Components/ParksList.razor.Tests.cs
@@ -19,5 +19,51 @@ namespace UnitTests.Components
 
         #endregion TestSetup
 
+        [Test]
+        public void ParkList_Default_Should_Return_Content()
+        {
+            // Arrange
+            //Creates a singleton of the file service
+            Services.AddSingleton<JsonFileParksService>(TestHelper.ParkService);
+
+            // Act
+            //Render the ParksList page
+            var page = RenderComponent<ParksList>();
+
+            // Get the Cards retrned
+            var result = page.Markup;
+
+            // Assert
+            //Ensure a valid Id is present
+            Assert.AreEqual(true, result.Contains("LAKE SAMMAMISH STATE PARK"));
+        }
+
+        #region SelectPark
+        [Test]
+        public void SelectPark_Valid_ID_Lake_Sammamish_Should_Return_Content()
+        {
+            // Arrange
+            Services.AddSingleton<JsonFileParksService>(TestHelper.ParkService);
+            var id = "MoreInfoButton_LAKE SAMMAMISH STATE PARK";
+
+            var page = RenderComponent<ParksList>();
+
+            // Find the Buttons (more info)
+            var buttonList = page.FindAll("Button");
+
+            // Find the one that matches the ID looking for and click it
+            var button = buttonList.First(m => m.OuterHtml.Contains(id));
+
+            // Act
+            button.Click();
+
+            // Get the markup to use for the assert
+            var pageMarkup = page.Markup;
+
+            // Assert
+            //ensure the acerage of "LAKE SAMMAMISH STATE PARK" is within the markup
+            Assert.AreEqual(true, pageMarkup.Contains("531"));
+        }
+        #endregion SelectPark
     }
 }
