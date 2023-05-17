d3d0599e51bacd5cb5bbefac464f51cf24d00af8
namcnamara <nicholas.anthony.mcnamara@gmail.com>
2023-05-16 17:27:43 -0700

    Added comments and adjusted function names in UpdateComment

---
+++ UnitTests/Components/UpdateComment.razor.Tests.cs
@@ -0,0 +1,32 @@
+﻿using Bunit;
+using NUnit.Framework;
+
+using LetsGoPark.WebSite.Components;
+using Microsoft.Extensions.DependencyInjection;
+using LetsGoPark.WebSite.Services;
+using System.Linq;
+using Castle.DynamicProxy.Generators.Emitters.SimpleAST;
+using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
+using Microsoft.VisualBasic;
+
+namespace UnitTests.Components
+{
+    /// <summary>
+    /// Unit tests for Homepage
+    /// </summary>
+    public class UpdateCommentTests : BunitTestContext
+    {
+        #region TestSetup
+        /// <summary>
+        /// Test Setup
+        /// </summary>
+        [SetUp]
+        public void TestInitialize()
+        {
+        }
+
+        #endregion TestSetup
+
+    }
+}
