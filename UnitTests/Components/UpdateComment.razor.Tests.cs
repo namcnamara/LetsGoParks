793617b9cf38aeb80bd6023e2f92a2cfb24fdca2
namcnamara <nicholas.anthony.mcnamara@gmail.com>
2023-05-17 00:19:21 -0700

    Add unit tests for page render using BUnit in UpdateComment.razor.Tests

--- UnitTests/Components/UpdateComment.razor.Tests.cs
+++ UnitTests/Components/UpdateComment.razor.Tests.cs
@@ -28,5 +28,38 @@ namespace UnitTests.Components
 
         #endregion TestSetup
 
+        #region ModelRendered
+        /// <summary>
+        /// This test uses valid parameters and should return true
+        /// It Creates a parkService from a mocked webhost,
+        /// then passes in values to the renderComponent function
+        /// </summary>
+        [Test]
+        public void UpdateComment_Page_Passed_Parameters_Should_Return_Page()
+        {
+            // Arrange
+            Services.AddSingleton<JsonFileParksService>(TestHelper.ParkService);
+            //Populate bunit component parameter dictionary: 
+            var commentIndex = ComponentParameter.CreateParameter(nameof(DeleteComment.CommentIndex), 0);
+            var parkId = ComponentParameter.CreateParameter(nameof(DeleteComment.SelectedParkId), "LAKE SAMMAMISH STATE PARK");
+            var name = ComponentParameter.CreateParameter(nameof(DeleteComment.Name), "Nick");
+            var datetime = ComponentParameter.CreateParameter(nameof(DeleteComment.Datetime), "2022-10-01");
+            var desc = ComponentParameter.CreateParameter(nameof(DeleteComment.Description), "Test description");
+            var token = ComponentParameter.CreateParameter(nameof(DeleteComment.Token), "0");
+            //Create parameter variable to hold component parameters
+            var parameters = new[]
+            {
+                commentIndex, parkId, name, datetime, desc, token
+            };
+
+            // Act
+            //Render webpage passing in parameters list
+            var page = RenderComponent<UpdateComment>(parameters);
+
+            // Assert
+            //Check to see if page is loaded in a valid state
+            Assert.NotNull(page);
+        }
+        #endregion ModelRendered
     }
 }
