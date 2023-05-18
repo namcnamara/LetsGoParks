44d1d81569dd17da063a29c65ca296f9d1861169
namcnamara <nicholas.anthony.mcnamara@gmail.com>
2023-05-17 17:48:02 -0700

    Created unit tests to ensure comments are correctly updated in UpdateComment.razor.tests

--- UnitTests/Components/UpdateComment.razor.Tests.cs
+++ UnitTests/Components/UpdateComment.razor.Tests.cs
@@ -61,5 +61,68 @@ namespace UnitTests.Components
             Assert.NotNull(page);
         }
         #endregion ModelRendered
+    
+    #region SubmitUpdatedComment
+    /// <summary>
+    /// This function checks to see if a comment was deleted on button click
+    /// It creates four string values for comment creation
+    /// It uses those strings to create BUnit parameters
+    /// It Creates a new comment for the specified park using the comment created previously
+    /// It gathers all comments for the specified park
+    /// It renders the webpage using BUnit parameters
+    /// It gathers the button, then preforms a Click() action
+    /// A new list of comments is gathered to ensure the second comment list is different from the first
+    /// </summary>
+    [Test]
+    public void SubmitUpdatedComment_Click_Delete_Button_Should_Remove_Comment_From_List()
+    {
+        // Arrange
+        Services.AddSingleton<JsonFileParksService>(TestHelper.ParkService);
+        string Name = "Nick";
+        string DateTime = "2022-10-01";
+        string Desc = "test descript";
+        string Token = "0";
+        string ParkId = "LAKE SAMMAMISH STATE PARK";
+        //Create comment array
+        var comment = new[] { Name, DateTime, Desc, Token };
+        //Create new comment in specified park:
+        TestHelper.ParkService.AddComment(ParkId, comment);
+        //Gather all existing comments for the park 
+        var comments = TestHelper.ParkService.GetParks().First(m => m.Id == ParkId).Comments;
+
+        //Populate bunit component parameter dictionary: 
+        var commentIndex = ComponentParameter.CreateParameter(nameof(DeleteComment.CommentIndex), 0);
+        var parkId = ComponentParameter.CreateParameter(nameof(DeleteComment.SelectedParkId), ParkId);
+        var name = ComponentParameter.CreateParameter(nameof(DeleteComment.Name), "John");
+        var datetime = ComponentParameter.CreateParameter(nameof(DeleteComment.Datetime), DateTime);
+        var desc = ComponentParameter.CreateParameter(nameof(DeleteComment.Description), Desc);
+        var token = ComponentParameter.CreateParameter(nameof(DeleteComment.Token), Token);
+
+        //Create parameter variable to hold component parameters
+        var parameters = new[]
+        {
+                commentIndex, parkId, name, datetime, desc, token
+            };
+
+        // Act
+        //Render webpage passing in parameters list
+        var page = RenderComponent<UpdateComment>(parameters);
+        // Get the Cards retrned
+        var result = page.Markup;
+
+        // Find the Buttons (more info)
+        var buttonList = page.FindAll("Button");
+        var button = buttonList.FirstOrDefault();
+        //Call the deleteComment function in the page
+        button.Click();
+        //Gather the comments in the current park again
+        var comments2 = TestHelper.ParkService.GetParks().First(m => m.Id == ParkId).Comments;
+
+            // Assert
+            //Ensure comments2 had an updated name 
+            Assert.AreNotEqual(comments[0], comments2[0]);
+
     }
+    #endregion SubmitUpdatedComment
+}
 }
