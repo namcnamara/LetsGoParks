04ad4fc3247670ecb60c97558a9a484a57f1e4f2
namcnamara <nicholas.anthony.mcnamara@gmail.com>
2023-05-22 17:50:27 -0700

    Created ParksSystemEnum

---
+++ src/Models/ParkSystemEnum.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LetsGoPark.WebSite.Models
+{
+    /// <summary>
+    /// This enum holds ParkSystems
+    /// </summary>
+    //Enum list holds the possiblilites for city, state, and national systems
+    public enum ParkSystemEnum
+    {
+        City = 0,
+        State = 1,
+        National = 2,
+    }
+
+    public static class ParkSystemEnumExtensions
+    {
+        //Displays the data of the Enum
+        public static string DisplayName(this ParkSystemEnum data)
+        {
+            //Return switch for string representation
+            return data switch
+            {
+                ParkSystemEnum.City => "City Parks",
+                ParkSystemEnum.State => "WA StatE Parks",
+                ParkSystemEnum.National => "National 2 Parks",
+
+                //Default value if nothing is specified
+                _ => "WA State Parks",
+            };
+        }
+    }
+}
