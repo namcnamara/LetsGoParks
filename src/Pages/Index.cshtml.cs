88a63f1faeff1368af8b8b3c30f9c06ef9ae101f
namcnamara <nicholas.anthony.mcnamara@gmail.com>
2023-05-23 22:03:12 -0700

    Added comments to Index.cshtml.cs

--- src/Pages/Index.cshtml.cs
+++ src/Pages/Index.cshtml.cs
@@ -10,21 +10,27 @@ namespace LetsGoPark.WebSite.Pages
 {
     public class IndexModel : PageModel 
     {
+        //Adds logger to class
         private readonly ILogger<IndexModel> _logger;
 
+        //Class constructor
         public IndexModel(ILogger<IndexModel> logger,
             JsonFileParksService parkService)
         {
+            //Assigns logger to class
             _logger = logger;
+            //Assigns instance of JsonFileParksService for class to use
             ParkService = parkService;
         }
         
-       
+       //Instance of JsonFileparksService
         public JsonFileParksService ParkService { get; }
+        //Enumerable of all parks in parks.json
         public IEnumerable<ParksModel> Parks { get; private set; }
 
         public void OnGet()
         {
+            //finds all parks in parks.json
             Parks = ParkService.GetParks();
         }
     }
