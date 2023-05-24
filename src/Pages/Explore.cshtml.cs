5e0ad0b14f90bad19f0ea0781800d3b53e3e3087
namcnamara <nicholas.anthony.mcnamara@gmail.com>
2023-05-23 22:03:03 -0700

    Added comments to Explore.cshtml.cs

--- src/Pages/Explore.cshtml.cs
+++ src/Pages/Explore.cshtml.cs
@@ -10,17 +10,22 @@ namespace LetsGoPark.WebSite.Pages
 {
     public class ExploreModel : PageModel
     {
+        //Adds logger to class
         private readonly ILogger<ExploreModel> _logger;
 
+        //Constructor for class
         public ExploreModel(ILogger<ExploreModel> logger,
             JsonFileParksService parkService)
         {
+            //assigns logger to class
             _logger = logger;
+            //assigns instance of ParkService to the class so it can access parks.json
             ParkService = parkService;
         }
 
-
+        //Instance of JsonfileParksService
         public JsonFileParksService ParkService { get; }
+        //Enumerable of all parks in parks.json
         public IEnumerable<ParksModel> Parks { get; private set; }
 
         /// <summary>
@@ -28,6 +33,7 @@ namespace LetsGoPark.WebSite.Pages
         /// </summary>
         public void OnGet()
         {
+            //Gets all parks upon start
             Parks = ParkService.GetParks();
         }
     }
