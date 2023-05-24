bd2f790d8a8f89930cf32d8e32c06aa61ec84ea9
namcnamara <nicholas.anthony.mcnamara@gmail.com>
2023-05-23 22:01:27 -0700

    Added comments to Contact.cshtml.cs

--- src/Pages/Contact.cshtml.cs
+++ src/Pages/Contact.cshtml.cs
@@ -5,10 +5,13 @@ namespace LetsGoPark.WebSite.Pages
 {
     public class ContactModel : PageModel
     {
+        //Adds logger to the class
         private readonly ILogger<ContactModel> _logger;
 
+        //Constructor for ContactModel
         public ContactModel(ILogger<ContactModel> logger)
         {
+            //Assigns logger to the class
             _logger = logger;
         }
 
