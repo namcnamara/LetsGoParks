3fda569c97d051332bbfb96b8bac49d1809d7534
namcnamara <nicholas.anthony.mcnamara@gmail.com>
2023-05-23 22:03:32 -0700

    Added comments to Privacy.cshtml.cs

--- src/Pages/Privacy.cshtml.cs
+++ src/Pages/Privacy.cshtml.cs
@@ -5,15 +5,22 @@ namespace LetsGoPark.WebSite.Pages
 {
     public class PrivacyModel : PageModel
     {
+        //Adds logger to class
         private readonly ILogger<PrivacyModel> _logger;
 
+        //Constructor class
         public PrivacyModel(ILogger<PrivacyModel> logger)
         {
+            //Assign logger to class
             _logger = logger;
         }
 
+        /// <summary>
+        /// Called upon page rendering to log 
+        /// </summary>
         public void OnGet()
         {
+            //logs successful loading of page to logger
             _logger.LogInformation("Request Privacy.cshtml");
         }
     }
