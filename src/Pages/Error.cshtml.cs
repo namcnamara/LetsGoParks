cfedf764df1c63e314dff2f0a67556eeeb5b3c13
namcnamara <nicholas.anthony.mcnamara@gmail.com>
2023-05-23 22:02:31 -0700

    Added comments to Error.cshtml.cs

--- src/Pages/Error.cshtml.cs
+++ src/Pages/Error.cshtml.cs
@@ -9,17 +9,24 @@ namespace LetsGoPark.WebSite.Pages
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public class ErrorModel : PageModel
     {
+        //Id of the request
         public string RequestId { get; set; }
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
 
+        //Adds a logger to the class
         private readonly ILogger<ErrorModel> _logger;
 
+        //Class constructor
         public ErrorModel(ILogger<ErrorModel> logger)
         {
+            //Assigns logger to class instance
             _logger = logger;
         }
 
+        /// <summary>
+        /// Gets the requestId of the current http request
+        /// </summary>
         public void OnGet()
         {
             RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier;
